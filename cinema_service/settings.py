6184c027a722dfe71d18ac2759b77b08a69d21b9
JustVov4uk <volodabudzan4@gmail.com>
2026-01-06 15:38:42 +0100

    Solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -139,7 +139,10 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": [
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
-    ]
+    ),
 }
--- user/urls.py
+++ user/urls.py
@@ -4,8 +4,8 @@ from user.views import CreateUserView, LoginUserView, ManageUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", LoginUserView.as_view(), name="login_token"),
-    path("me/", ManageUserView.as_view(), name="me"),
+    path("login/", LoginUserView.as_view(), name="login"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "user"
